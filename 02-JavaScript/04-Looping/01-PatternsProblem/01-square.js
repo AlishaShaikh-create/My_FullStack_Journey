69f5c7e0c3402458dd2ecb89ca681f7707c8d8dc
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-02-24 22:29:32 +0530

    right angle triangle

--- 02-JavaScript/04-Looping/01-PatternsProblem/01-square.js
+++ 02-JavaScript/04-Looping/01-PatternsProblem/01-square.js
@@ -8,3 +8,13 @@ for(let i=0;i<=5;i++){
     }
     console.log(temp)
 }
+
+// right angle triangle
+
+for(let i=1;i<=5;i++){
+    let temp=''
+    for (let j=1;j<=i;j++){
+        temp+='*'
+    }
+    console.log(temp)
+}
