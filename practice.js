81fb4beeb0d39874caa519f336449807b3df6f50
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-02-27 19:07:37 +0530

    reverse the number

--- practice.js
+++ practice.js
@@ -1,26 +1,21 @@
-// // Even or odd problem
-
-// import read from 'readline-sync'
-
-// let number=read.questionInt("Enter the number:")
-// if(number%2==0){
-//     console.log("Even")
-// }else{
-//     console.log("odd")
-// }
 
 
-// Largest of 3 Numbers
+// Reverse the number
 
-let num1=read.questionInt("Enter the 1st number:");
-let num2 =read.questionInt("Enter the second Number:");
-let num3=read.questionInt("Enter the 3rd number:");
+let num=12345
+// console.log(num%10)
+// let digit=Math.floor(num/10)
+// console.log(digit)
 
-if((num1>num2) && (num1>num3)){
-    console.log("Number 1 is greater", num1)
-}else if((num2>num3) && (num2 > num1)){
-    console.log("Number 2 is greater",num2);
-}else{
-    console.log("Number 3 is greater",num3)
+let digit=""
+
+while(num>0){
+    let value=num%10 
+    num = Math.floor(num/10)
+    digit+=value
+
 }
+console.log(digit)
+
+
 
