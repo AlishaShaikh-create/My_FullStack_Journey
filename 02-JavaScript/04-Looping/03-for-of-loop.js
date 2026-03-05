6cd2854483f36cf9f448c1ae3e4c4c61bdde4e14
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-05 21:38:21 +0530

    for of loop

---
+++ 02-JavaScript/04-Looping/03-for-of-loop.js
@@ -0,0 +1,10 @@
+// for of loop in js
+let arr=[1,2,3,4,5]
+for(let ele of arr){
+    console.log(ele)
+}
+
+let fruits=['apple','banana','grapes']
+for(let fruit of fruits){
+    console.log(fruit)
+}
