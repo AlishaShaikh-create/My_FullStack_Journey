1a5026f15d392dc99020842dfdb8463a21c1d818
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-31 11:15:17 +0530

    async

--- 02-JavaScript/16-Async/01-Promise.js
+++ 02-JavaScript/16-Async/01-Promise.js
@@ -1,50 +1,50 @@
-// to overcome the callback we use the promise
+// // to overcome the callback we use the promise
 
-//PROMISE: The Promise object represent the eventual completion or failure of an async operation and its resulting value
+// //PROMISE: The Promise object represent the eventual completion or failure of an async operation and its resulting value
 
-// Promise is object
+// // Promise is object
 
-function savetoDb(data){
-    return new Promise((success,failure)=>{
-        let internetSpeed = Math.floor(Math.random()*10 +1)
-        if(internetSpeed > 4){
-            success('success: The data was saved')
-        }
-        else{
-            failure("failure: The data was not saved")
-        }
-    })
-}
-let result = savetoDb("alisha")
-console.log(result)
+// function savetoDb(data){
+//     return new Promise((success,failure)=>{
+//         let internetSpeed = Math.floor(Math.random()*10 +1)
+//         if(internetSpeed > 4){
+//             success('success: The data was saved')
+//         }
+//         else{
+//             failure("failure: The data was not saved")
+//         }
+//     })
+// }
+// let result = savetoDb("alisha")
+// console.log(result)
 
-// Creating a promise 
-let res = new Promise((resolve,reject)=>{
-    let success =false
-    if(success){
-        resolve("task completed")
-    }else{
-        reject("task not completed")
-    }
-})
+// // Creating a promise 
+// let res = new Promise((resolve,reject)=>{
+//     let success =false
+//     if(success){
+//         resolve("task completed")
+//     }else{
+//         reject("task not completed")
+//     }
+// })
 
-console.log(res)
+// console.log(res)
 
-// difference between the resolve and reject
+// // difference between the resolve and reject
 
-const p = new Promise((resolve,reject)=>{
-    resolve("hello")
-})
-console.log(p)
+// const p = new Promise((resolve,reject)=>{
+//     resolve("hello")
+// })
+// console.log(p)
 
-p.then((result)=>{
-    console.log(result)
-})
+// p.then((result)=>{
+//     console.log(result)
+// })
 
 
 // Creating the promise and then and catch method
 let output = new Promise((resolve,reject)=>{
-    let number = Math.floor(Math.random*10+1)
+    let number = Math.floor(Math.random()*10+1)
     if(number > 4){
         resolve("The number is greater than four")
     }else{
@@ -55,14 +55,29 @@ let output = new Promise((resolve,reject)=>{
 
 
 
-output.then((r)=>{
-    console.log(r)
-})
-.catch((r)=>{
-    console.log(r)
-})
+// output.then((r)=>{
+//     console.log(r)
+// })
+// .catch((r)=>{
+//     console.log(r)
+// })
+
 
 // promise chaining
 
+output.then((out)=>{
+    console.log(out)
+    console.log("promise 1 resolved")
+    return output
 
+})
+    .then((out)=>{
+        console.log(out)
+    console.log("promise 2 resolved")
+})
+
+.catch((out)=>{
+    console.log(out)
+    console.log("some promise rejected")
+})
 
---
+++ 02-JavaScript/16-Async/02-Async.js
@@ -0,0 +1,7 @@
+// Async Function -> return the promise 
+// now u can apply the .then and .catch method
+
+async function greet(){
+    return "hello World"
+}
+console.log(greet()) // output : return the promise
